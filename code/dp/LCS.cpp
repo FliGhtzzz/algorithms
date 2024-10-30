cfec3c51deb3914d08ada4abf3d29d24fdf90427
flightzzz <tw0729brad@gmail.com>
2024-10-30 21:50:57 +0800

    24/10/30

--- code/dp/LCS.cpp
+++ code/dp/LCS.cpp
@@ -12,9 +12,7 @@ signed main() {
         for(int j=1;j<m;j++) {
             if(a[i]==b[j]) dp[i][j]=dp[i-1][j-1]+1; 
             else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-            cerr<<dp[i][j]<<" ";
         }
-        cerr<<'\n';
     }
     cout<<dp[l-1][m-1];
 }
